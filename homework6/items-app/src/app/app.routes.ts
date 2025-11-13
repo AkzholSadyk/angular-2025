dfda1b9fba6de8ba67bfca55eac5da61bd214f9b
Akzhol Sadyk <akzholsadyk12@gmail.com>
2025-11-13 18:22:33 +0500

    HW6

---
+++ homework6/items-app/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Login } from './pages/login/login';
+import { ItemsList } from './components/items-list/items-list';
+import { ItemDetails } from './components/item-details/item-details';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'login', component: Login },
+  { path: 'items', component: ItemsList },
+  { path: 'items/:id', component: ItemDetails },
+  { path: '**', redirectTo: '' }
+];
